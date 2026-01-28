4234ae7426ea7e44b3ebdb89a2813d9bc0d5448c
nishthaasingh831 <nishthaasingh831@gmail.com>
2026-01-28 11:46:34 +0530

    [Nishtha] Control-flows-Level1

---
+++ Control-flows-Level-1/Logicalop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Logicalop
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean a=true;
+		boolean b=false;
+		if(a&&b)
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+	}
+}	
+		
