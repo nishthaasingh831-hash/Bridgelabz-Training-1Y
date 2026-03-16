f80a06b46f306535545c9f3b6fab964d0931600d
nishthaasingh831 <nishthaasingh831@gmail.com>
2026-03-17 00:44:33 +0530

    [Nishtha] Practiceproblems Added

---
+++ Practiceproblems/Volume.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Volume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float radius = sc.nextFloat();
+        float height = sc.nextFloat();
+        double volume = 3.14 * radius * radius * height;
+        System.out.println("Volume of cylinder = " + volume);
+    }
+}
